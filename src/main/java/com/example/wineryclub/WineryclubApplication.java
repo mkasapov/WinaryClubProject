bcf90024f976c5b3a2b83abb476d6bcbcdc4dba0
mkasapov <martin.kasapov.mk@gmail.com>
2019-04-18 20:46:17 +0300

    First commit

---
+++ src/main/java/com/example/wineryclub/WineryclubApplication.java
@@ -0,0 +1,13 @@
+package com.example.wineryclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineryclubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WineryclubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wineryclub/WineryclubApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.wineryclub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WineryclubApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
